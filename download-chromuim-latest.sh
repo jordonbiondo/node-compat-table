69267e3afb5a04ab1fd71493babf4871f3d2047b
William Kapke <william.kapke@gmail.com>
2017-02-26 22:12:15 +0000

    correct filenames

--- download-chromuim-latest.sh
+++ download-chromuim-latest.sh
@@ -4,7 +4,7 @@ DIR=${1:-'./chromium-latest'}
 
 if [[ $? -eq 0 && $LATEST ]]; then
   echo "Downloading: $LATEST"
-  curl -L $LATEST > ./chromium-latest.zip
+  curl -L --progress-bar $LATEST > ./chromium-latest.zip
 
   if [[ $? -eq 0 ]]; then
     unzip -q -u -o -d $DIR ./chromium-latest.zip
